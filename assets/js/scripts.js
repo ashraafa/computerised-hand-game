5b44e8641bb9f4a7c1aff5188a51f9327ea4e759
ashraafa <ashraaf.alexander@gmail.com>
2021-10-29 12:50:46 +0000

    Add notes to js script for game functions to be added

---
+++ assets/js/scripts.js
@@ -0,0 +1,19 @@
+//Set inactive status to game controls after DOM loads
+
+//Add event listeners to the game control buttons 
+
+//Define array for player options and random game selection
+
+//Main function to run game and calculate result
+
+//Display result
+
+//Increment round score
+
+//Increment game score
+
+//Continue Game
+
+//Display game rules
+
+//Quit game, deactive game controls and reset scores
